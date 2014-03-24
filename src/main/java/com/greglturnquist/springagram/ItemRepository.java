568a49d86d968d3adfa8cea5b3507a3ac847669c
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-24 12:23:53 -0500

    Split js into separate file

--- src/main/java/com/greglturnquist/springagram/ItemRepository.java
+++ src/main/java/com/greglturnquist/springagram/ItemRepository.java
@@ -3,6 +3,6 @@ package com.greglturnquist.springagram;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource()
 public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
 }
---
+++ src/main/resources/static/upload.js
@@ -0,0 +1,29 @@
+(function() {
+    var name, bytes;
+    var itemRepository = sdr.repositoriesFactory(null).itemRepository;
+
+    function readImage(input) {
+        if (input.files && input.files[0]) {
+            var FR = new FileReader();
+            FR.onloadend = function () {
+                name = input.files[0].name;
+                bytes = FR.result;
+            }
+            FR.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('#file').change(function () {
+        readImage(this);
+    });
+
+    $('#upload').submit(function (e) {
+        e.preventDefault();
+        itemRepository.create({
+            name: name,
+            image: bytes
+        }).done(function () {
+            window.location.reload();
+        })
+    });
+})();
