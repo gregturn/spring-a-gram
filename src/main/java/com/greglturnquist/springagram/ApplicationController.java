dcb0308c294848932f761dda41462e9a177e29c0
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-24 22:50:24 -0500

    Rename HomeController to ApplicationController & add /image/{id}

---
+++ src/main/java/com/greglturnquist/springagram/ApplicationController.java
@@ -0,0 +1,41 @@
+package com.greglturnquist.springagram;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * This is the web controller that contains web pages and other custom end points.
+ */
+@Controller
+public class ApplicationController {
+
+	@Autowired
+	ItemRepository repository;
+
+	/**
+	 * Serve up the home page
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/index")
+	public String index(Model model) {
+
+		model.addAttribute("items", repository.findAll());
+		return "index";
+	}
+
+	/**
+	 * Provide the RESTful means to fetch an individual image's data based on id
+	 * @param id
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/image/{id}")
+	public String image(@PathVariable Long id) {
+		return repository.findOne(id).getImage();
+	}
+}
