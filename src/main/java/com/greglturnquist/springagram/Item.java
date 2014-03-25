3047afbda85dc965097f6108ea2f815952acb66a
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-24 22:33:32 -0500

    Remove extraneous imports

--- src/main/java/com/greglturnquist/springagram/Item.java
+++ src/main/java/com/greglturnquist/springagram/Item.java
@@ -1,8 +1,5 @@
 package com.greglturnquist.springagram;
 
-import org.springframework.hateoas.ResourceSupport;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
