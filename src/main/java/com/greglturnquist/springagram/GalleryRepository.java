6ce66b7b966328c700ab99ecd9b85e88e682a2b0
Greg Turnquist <gturnquist@gopivotal.com>
2014-04-08 00:03:36 -0500

    Simplify hypermedia endpoints
    Using the PagingAndSortingRepository interface introduces {?paging,etc.}, which cujoJS can't handle. For now, swapped it over to CrudRepository to remove these extra options

--- src/main/java/com/greglturnquist/springagram/GalleryRepository.java
+++ src/main/java/com/greglturnquist/springagram/GalleryRepository.java
@@ -1,8 +1,8 @@
 package com.greglturnquist.springagram;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface GalleryRepository extends PagingAndSortingRepository<Gallery, Long> {
+public interface GalleryRepository extends CrudRepository<Gallery, Long> {
 }
--- src/main/java/com/greglturnquist/springagram/ItemRepository.java
+++ src/main/java/com/greglturnquist/springagram/ItemRepository.java
@@ -1,12 +1,12 @@
 package com.greglturnquist.springagram;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 @RepositoryRestResource()
-public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+public interface ItemRepository extends CrudRepository<Item, Long> {
 
 	List<Item> findByGalleryIsNull();
 
