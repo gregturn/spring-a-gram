09e2760880ca6a579ef19ea83e47d3b9c9f41cd4
Greg Turnquist <gturnquist@gopivotal.com>
2014-04-21 16:59:53 -0500

    Work on pictures part of display
    Got the general flow of the windows working.
    
    Made pictures dynamically load.
    
    Coded solution to then delete pictures, drop them from the data structures, and surf back to list of pics.

--- src/main/resources/static/mobile.js
+++ src/main/resources/static/mobile.js
@@ -20,6 +20,7 @@
             .chain(defaultRequest, {headers: {'Accept': 'application/hal+json'}});
         var items = {};
         var galleries = {};
+        var currentItem;
 
         /* Convert a single or array of resources into "URI1\nURI2\nURI3..." */
         var uriListConverter = {
@@ -53,41 +54,6 @@
             }
         }
 
-        /* Search for a given item in the table of unlinked images */
-        function findUnlinkedItem(item) {
-            return $('#images tr[data-uri="' + item._links.self.href + '"]');
-        }
-
-        /* Search for a given item in the gallery's table */
-        function findLinkedItem(item) {
-            return $('#gallery tr[data-uri="' + item._links.self.href + '"]');
-        }
-
-        /* Delete the picture from storage and remove from the screen */
-        function deletePic(item) {
-            api({ method: 'DELETE', path: item._links.self.href }).then(function(response) {
-                findUnlinkedItem(item).remove();
-                delete items[item._links.self.href];
-            });
-        }
-
-        /* Move the picture from table of unlinked items to its gallery's table */
-        function addToSelectedGallery(item) {
-            if (currentGallery === undefined) {
-                return;
-            }
-
-            api({
-                method: 'PUT',
-                path: item._links.gallery.href,
-                entity: currentGallery,
-                headers: {'Content-Type': 'text/uri-list'}
-            }).then(function() {
-                $('#gallery table table').append(createItemRowForGallery(item, currentGallery));
-                findUnlinkedItem(item).remove();
-            });
-        }
-
         /* Take either a JSON or URI version of a resource, and extract it's ID */
         function id(resource) {
             if (typeof resource === "string") {
@@ -96,96 +62,6 @@
                 var parts = resource._links.self.href.split("/");
             }
             return parts[parts.length - 1];
-        }
-
-        /* Unlink an item from it's gallery, and move it to the table of unlinked items */
-        function removePicByResource(item, gallery) {
-            if (gallery === undefined || item === undefined) {
-                return;
-            }
-
-            api({
-                method: 'DELETE',
-                path: item._links.gallery.href
-            }).then(function(response) {
-                findLinkedItem(item).remove();
-                $('#images table').append(createItemRow(item));
-            });
-        }
-
-        /* Create a new table row for a item based on its gallery */
-        function createItemRowForGallery(item, gallery) {
-            var row = $('<li></li>')
-                .attr('data-uri', item._links.self.href)
-                .text(item.name);
-
-            row.append($('<img class="ui-li-thumb">').attr('src', item.image));
-
-            row.append(
-                $('<button>Remove</button>')
-                    .attr('data-gallery-uri', gallery._links.self.href)
-                    .attr('data-uri', item._links.self.href)
-
-            );
-
-            return row;
-        }
-
-        /* Draw the gallery table from scratch */
-        function drawGalleryTable(data) {
-            var list = $('<ul data-role="listview"></ul>');
-            data.forEach(function (gallery) {
-                var row = $('<li></li>')
-                    .attr('data-uri', gallery._links.self.href);
-
-                var label = $('<label></label>').text(gallery.description);
-                label.append(
-                    $('<input type="radio" name="gallery">').click(function() {
-                        currentGallery = gallery;
-                    })
-                );
-                row.append(label);
-
-                var nestedList = $('<ul></ul>');
-                api({
-                    method: 'GET',
-                    path: gallery._links.items.href
-                }).then(function(response) {
-                    if (response.entity._embedded) {
-                        response.entity._embedded.items.forEach(function (item) {
-                            items[item._links.self.href] = item;
-                            nestedList.append(createItemRowForGallery(item, gallery));
-                        });
-                    }
-                });
-
-                row.append(nestedList);
-
-                list.append(row);
-            });
-            $('#gallery').append(list).trigger('create');
-        }
-
-        /* Create a new table row for an unlinked item */
-        function createItemRow(item) {
-            var row = $('<li></li>').attr('data-uri', item._links.self.href);
-
-            var href = $('<a href="#add-to-gallery" data-rel="popup"></a>');
-
-            href.append($('<img>').attr('src', item.image));
-
-            href.append($('<h2></h2>').text(item.name));
-
-            row.append(href);
-
-            row.append($('<a href="#remove" data-rel="popup"></a>'));
-
-            return row;
-        }
-
-        /* Append an item's table row to the image table */
-        function addItemRow(item) {
-            $('#images ul').append(createItemRow(item));
         }
 
         function follow(relArray) {
@@ -206,6 +82,20 @@
         }
 
         /* When the page is loaded, run/register this set of code */
+        function addItemRow(item) {
+            var list = $('<li></li>').attr('data-uri', item._links.self.href);
+
+            list.append(
+                $('<a data-rel="popup" data-transition="fade" data-position-to="window"></a>')
+                    .attr('href', '#view')
+                    .append($('<img />').attr('src', item.image))
+            );
+
+            list.append($('<a href="#pic_ops" data-rel="popup" data-transition="flow" data-icon="gear"></a>'));
+
+            return list;
+        }
+
         $(function() {
             /* Listen for picking a file */
             $('#file').change(function () {
@@ -259,21 +149,48 @@
                 }
             });
 
-            follow(['galleries', 'galleries']).then(function(response) {
-                response.forEach(function(gallery) {
-                    galleries[gallery._links.self.href] = gallery;
+            $('#piclist').on('click', function(e) {
+                console.log(e);
+                console.log(e.target);
+                console.log(e.target.parentNode);
+                if (e.target.tagName === 'IMG') {
+                    currentItem = items[e.target.parentNode.parentNode.dataset['uri']];
+                }
+                if (e.target.tagName === 'A') {
+                    currentItem = items[e.target.parentNode.dataset['uri']];
+                }
+                $('#view img').attr('src', currentItem.image);
+            });
+
+            $('#deleteConfirmed').on('click', function(e) {
+                api({ method: 'DELETE', path: currentItem._links.self.href }).then(function(response) {
+                    $('#piclist li[data-uri="' + currentItem._links.self.href + '"').remove();
+                    delete items[currentItem._links.self.href];
+                    currentItem = undefined;
                 });
-                drawGalleryTable(response);
             })
 
+//            follow(['galleries', 'galleries']).then(function(response) {
+//                response.forEach(function(gallery) {
+//                    galleries[gallery._links.self.href] = gallery;
+//                });
+//                drawGalleryTable(response);
+//            })
+
             follow(['items', 'search', 'findByGalleryIsNull', 'items']).then(function(response) {
-                var list = $('<ul data-role="listview" data-split-icon="delete"></ul>');
-                $('#images').append(list);
+                var piclist = $('#piclist');
                 response.forEach(function(item) {
                     items[item._links.self.href] = item;
-                    addItemRow(item);
+                    piclist.append(addItemRow(item));
                 });
-                $('#images').trigger('create');
+                piclist.listview('refresh');
+//                var list = $('<ul data-role="listview" data-split-icon="delete"></ul>');
+//                $('#images').append(list);
+//                response.forEach(function(item) {
+//                    items[item._links.self.href] = item;
+//                    addItemRow(item);
+//                });
+//                $('#images').trigger('create');
             });
         })
 
