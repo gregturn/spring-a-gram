0a72812be1aa6724b66582ac3bf2124d31a393a0
Greg Turnquist <gturnquist@gopivotal.com>
2014-11-04 16:44:20 -0600

    Hacking

--- src/main/resources/static/run.js
+++ src/main/resources/static/run.js
@@ -2,8 +2,7 @@
 
 requirejs.config({
 	paths: {
-		'jquery': 'jquery/dist/jquery',
-		'jquery-mobile': 'jquery-mobile-bower/js/jquery.mobile-1.4.2'
+		'jquery': 'jquery/dist/jquery'
 	},
 	packages: [
 		{ name: 'when', location: 'when', main: 'when' },
@@ -14,18 +13,7 @@ requirejs.config({
 			'css': 'require-css/css'
 		}
 	},
-	shim: {
-		'jquery-mobile': {
-			deps: [ 'jquery' ],
-			exports: '$'
-		}
-	},
-	deps: isMobile() ? [ 'app/mobile' ] : [ 'app/main' ]
+	deps: [ 'app/main' ]
 });
-
-function isMobile () {
-	var html = document.documentElement;
-	return html.getAttribute('data-device') === 'mobile';
-}
 
 }());
