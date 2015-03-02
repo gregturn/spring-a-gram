166254814d1815980a5696019169bbd914da39e5
Roy Clarkson <roy@clarkson.org>
2015-03-02 17:12:53 -0600

    Use @SpringBootApplication

--- src/main/java/com/greglturnquist/springagram/Application.java
+++ src/main/java/com/greglturnquist/springagram/Application.java
@@ -3,17 +3,13 @@ package com.greglturnquist.springagram;
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
+@SpringBootApplication
 @EnableJpaRepositories
 @Import(CustomizedRestMvcConfiguration.class)
-@EnableAutoConfiguration
-@ComponentScan
 public class Application {
 
 	public static void main(String[] args) throws IOException {
