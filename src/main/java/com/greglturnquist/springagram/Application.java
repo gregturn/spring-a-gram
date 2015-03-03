cbe2e5316206b3aabe0d52c5ccc1d24e125d05a0
Roy Clarkson <roy@clarkson.org>
2015-03-03 17:51:33 -0600

    Spring Boot enables JPA support

--- src/main/java/com/greglturnquist/springagram/Application.java
+++ src/main/java/com/greglturnquist/springagram/Application.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @Import(CustomizedRestMvcConfiguration.class)
 public class Application {
 
