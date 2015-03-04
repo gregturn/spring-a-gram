579af57bab5ca017d940bb3419dea176a85a42ad
Greg Turnquist <gturnquist@gopivotal.com>
2015-03-04 16:08:35 -0600

    Use Owner projection to inline owner of an image

--- src/main/java/com/greglturnquist/springagram/Owner.java
+++ src/main/java/com/greglturnquist/springagram/Owner.java
@@ -1,10 +1,15 @@
 package com.greglturnquist.springagram;
 
 import org.springframework.data.rest.core.config.Projection;
+import org.springframework.hateoas.Link;
 
 @Projection(name = "owner", types = Item.class)
 public interface Owner {
 
 	public User getUser();
+
+	public String getImage();
+
+	public Link getHtmlUrl();
 
 }
