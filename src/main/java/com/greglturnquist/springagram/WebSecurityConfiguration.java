4377d617b11b9d399aac514b64f7f1ddb310d70e
Roy Clarkson <roy@clarkson.org>
2015-03-05 18:09:40 -0600

    Add support for OAuth authorized clients to access REST API
    Resolves #47

--- src/main/java/com/greglturnquist/springagram/WebSecurityConfiguration.java
+++ src/main/java/com/greglturnquist/springagram/WebSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.greglturnquist.springagram;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +25,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(User.PASSWORD_ENCODER);
+	}
+
+	// Needed by Spring Security OAuth
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 	@Override
