5307547337804c5d301698f92e13b7f21f669726
Roy Clarkson <roy@clarkson.org>
2015-08-14 17:20:45 -0500

    Enable Eureka on MongoDB file service

--- spring-a-gram-mongodb-fileservice/src/main/java/com/greglturnquist/springagram/fileservice/mongodb/MongoDbFileServiceApplication.java
+++ spring-a-gram-mongodb-fileservice/src/main/java/com/greglturnquist/springagram/fileservice/mongodb/MongoDbFileServiceApplication.java
@@ -2,8 +2,10 @@ package com.greglturnquist.springagram.fileservice.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MongoDbFileServiceApplication {
 
     public static void main(String[] args) {
